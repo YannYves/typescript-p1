1a848fcee0e85e798de0cf82e4442fd4989bd4f6
yann <yanndubois4@gmail.com>
2021-06-05 12:29:05 +0200

    refactor : use of es5 module

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,8 @@
+import { ProjectInput } from "./components/project-input.js";
+import { ProjectList } from "./components/project-list.js";
+
 // Project Type
 
-// namespace features
-
-/// <reference path='components/project-input.ts'/>
-/// <reference path='components/project-list.ts'/>
-
-namespace App {
-  new ProjectInput();
-  new ProjectList("active");
-  new ProjectList("finished");
-}
+new ProjectInput();
+new ProjectList("active");
+new ProjectList("finished");
